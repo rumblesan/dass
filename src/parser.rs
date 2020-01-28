0a9fb3e6b47c97f1534081c7c5b7a61bf15be6b6
Guy John <guy@rumblesan.com>
2020-01-28 10:20:26 +0000

    Create parser trait to allow using lexer as token stream

--- src/parser.rs
+++ src/parser.rs
@@ -5,31 +5,51 @@ use std::result::Result;
 use std::vec::IntoIter;
 
 use super::error::ParserError;
+use super::lexer::DassLexer;
 use super::tokens::TokenData;
 
-pub struct DassParser<T>
+pub trait DassParser<T>
+where
+    T: Clone + Display,
+{
+    fn eof(&mut self) -> bool;
+
+    fn la1(&mut self, tag: T) -> bool;
+
+    fn match_token(&mut self, tag: T) -> Result<TokenData<T>, ParserError>;
+
+    fn pop_token(&mut self) -> Result<TokenData<T>, ParserError>;
+}
+
+pub struct DassVecStreamTokenParser<T>
 where
     T: Clone + Display,
 {
     tokens: Peekable<IntoIter<TokenData<T>>>,
 }
 
-impl<T> DassParser<T>
+impl<T> DassVecStreamTokenParser<T>
 where
-    T: Clone + Display + Eq,
+    T: Clone + Display,
 {
-    pub fn from_vec(tokens: Vec<TokenData<T>>) -> Self {
-        DassParser {
+    pub fn new(tokens: Vec<TokenData<T>>) -> Self {
+        DassVecStreamTokenParser {
             tokens: tokens.into_iter().peekable(),
         }
     }
-    pub fn eof(&mut self) -> bool {
+}
+
+impl<T> DassParser<T> for DassVecStreamTokenParser<T>
+where
+    T: Clone + Display + Eq,
+{
+    fn eof(&mut self) -> bool {
         self.tokens.peek().is_none()
     }
-    pub fn la1(&mut self, tag: T) -> bool {
+    fn la1(&mut self, tag: T) -> bool {
         self.tokens.peek().map_or(false, |t| t.tag == tag)
     }
-    pub fn match_token(&mut self, tag: T) -> Result<TokenData<T>, ParserError> {
+    fn match_token(&mut self, tag: T) -> Result<TokenData<T>, ParserError> {
         self.tokens
             .next()
             .map_or(Err(ParserError::end_of_stream(&tag)), |t| {
@@ -40,11 +60,67 @@ where
                 }
             })
     }
-    pub fn pop_token(&mut self) -> Result<TokenData<T>, ParserError> {
+    fn pop_token(&mut self) -> Result<TokenData<T>, ParserError> {
         self.tokens.next().ok_or(ParserError::new(
             String::from("To Pop token"),
             String::from("End of Stream"),
             None,
         ))
+    }
+}
+
+pub struct DassTokenParser<'a, T>
+where
+    T: Clone + Display,
+{
+    lexer: Peekable<DassLexer<'a, T>>,
+}
+
+impl<'a, T> DassTokenParser<'a, T>
+where
+    T: Clone + Display,
+{
+    pub fn new(lexer: DassLexer<'a, T>) -> Self {
+        DassTokenParser {
+            lexer: lexer.peekable(),
+        }
+    }
+}
+
+impl<'a, T> DassParser<T> for DassTokenParser<'a, T>
+where
+    T: Clone + Display + Eq,
+{
+    fn eof(&mut self) -> bool {
+        self.lexer.peek().is_none()
+    }
+    fn la1(&mut self, tag: T) -> bool {
+        self.lexer.peek().map_or(false, |res| match res {
+            Err(_) => false,
+            Ok(t) => t.tag == tag,
+        })
+    }
+    fn match_token(&mut self, tag: T) -> Result<TokenData<T>, ParserError> {
+        self.lexer
+            .next()
+            .map_or(Err(ParserError::end_of_stream(&tag)), |res| {
+                res.and_then(|t| {
+                    if t.tag != tag {
+                        Err(ParserError::unexpected_token(tag, t))
+                    } else {
+                        Ok(t)
+                    }
+                })
+            })
+    }
+    fn pop_token(&mut self) -> Result<TokenData<T>, ParserError> {
+        match self.lexer.next() {
+            Some(t) => t,
+            None => Err(ParserError::new(
+                String::from("To Pop token"),
+                String::from("End of Stream"),
+                None,
+            )),
+        }
     }
 }
